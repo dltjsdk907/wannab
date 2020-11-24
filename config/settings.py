600e66e99294e91982fbd4f664143461d4ab7c6a
dltjsdk907 <dltjsdk907@nate.com>
2020-11-24 20:11:09 +0900

    wannab

--- config/settings.py
+++ config/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = 'j3y#elp0^sd9cb$)z(6@#)e9*h*6bf7a+_94qx@igqh66$x7hj'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["*", "http://localhost:8000"]
 
 
 
