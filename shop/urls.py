5398929d883f71a05c1fcc52126d45ae9c060b82
dltjsdk907 <dltjsdk907@nate.com>
2020-11-26 03:49:41 +0900

    wannab

--- shop/urls.py
+++ shop/urls.py
@@ -6,8 +6,6 @@ app_name = 'shop'
 urlpatterns = [
     path('', product_in_category,
          name='product_all'),  		# 카테고리 지정 없이, 모든 상품을 조회
-    # path('<slug:category_slug>/', product_in_category, # !!! 한글 슬러그 문제
-    #      name='product_in_category'), # 카테고리 지정하여, 해당 상품을 조회
     path('<category_slug>/', product_in_category,
          name='product_in_category'),   # 카테고리 지정하여, 해당 상품을 조회
     path('<int:id>/<product_slug>/', product_detail,
--- shop/views.py
+++ shop/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import TemplateView
 
@@ -7,21 +8,46 @@ from cart.forms import AddProductForm
 from cart.cart import Cart
 
 
+# def product_in_category(request, category_slug=None):
+#     # 맥락변수 3종을 목록 화면으로 전달
+#     current_category = None
+#     categories = Category.objects.all()
+#     cart = Cart(request)
+#     products = Product.objects.filter(available_display=True)
+#     if category_slug:
+#         current_category = get_object_or_404(Category, slug=category_slug)
+#         products = products.filter(category=current_category)
+#
+#     return render(request, 'shop/list.html',
+#                   {'current_category': current_category,
+#                    'categories': categories,
+#                    'products': products,
+#                    'cart': cart})
+
+
 def product_in_category(request, category_slug=None):
     # 맥락변수 3종을 목록 화면으로 전달
     current_category = None
     categories = Category.objects.all()
-    cart = Cart(request)
     products = Product.objects.filter(available_display=True)
     if category_slug:
         current_category = get_object_or_404(Category, slug=category_slug)
         products = products.filter(category=current_category)
+    page = request.GET.get('page', 1)
+    paginator = Paginator(products, 9)
+    try:
+        products = paginator.page(page)
+    except PageNotAnInteger:
+        products = paginator.page(1)
+    except EmptyPage:
+        products = paginator.page(paginator.num_pages)
 
+    cart = Cart(request)
     return render(request, 'shop/list.html',
                   {'current_category': current_category, # 지정된 카테고리 객체
                    'categories': categories,             # 모든 카테고리 객체
-                   'products': products,
-                   'cart': cart})                # 모든 또는 지정된 카테고리의 상품
+                   'products': products,                 # 모든 또는 지정된 카테고리의 상품
+                   'cart': cart})
 
 
 @login_required
